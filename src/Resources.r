39e3930ae0a0627d1973b690a851e2d87fb4e227
Jay Garcia <jesusdgarcia@gmail.com>
2019-03-01 15:21:26 -0500

    Adding Audio files

--- src/Resources.r
+++ src/Resources.r
@@ -10,3 +10,19 @@ BITMAP game_sprites.bmp
 BITMAP game.bmp
 # always splash.bmp last!!!!
 BITMAP splash.bmp
+
+
+PATH ../resources/Audio
+RAW EmptySong.xm
+RAW GameOver.xm
+RAW Intro.xm
+RAW SFX_bounce_off_player.wav
+RAW SFX_bounce_wall.wav
+RAW SFX_kill_block.wav
+RAW SFX_new_ball.wav
+RAW SFX_start_game.wav
+RAW Stage_1.xm
+RAW Stage_2.xm
+RAW Stage_3.xm
+RAW Stage_4.xm
+RAW Stage_5.xm
